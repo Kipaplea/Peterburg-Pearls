ebc008f8fbc10c8b700558f598eab512fc885863
Kipaplea <kipaplea@gmail.com>
2024-11-05 10:51:06 +0300

    Первый коммит.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide =0;
+
+function showSlide(n) {
+ slides.forEach((slide) => {
+ slide.style.display = 'none';
+ });
+ slides[n].style.display = 'block';
+ slider.style.transform = `translateX(0)`;
+}
+
+function nextSlide() {
+ currentSlide = (currentSlide +1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide -1 + slides.length) % slides.length;
+ showSlide(currentSlide);
+}
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+showSlide(0); 
+
+// ----------------------------------------------------------------------
+
